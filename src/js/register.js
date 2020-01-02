b422c0ed2c062048ad84ffed0f52678ff34c0df4
kong_1995 <1132154220@qq.com>
2020-01-02 18:01:55 +0800

    注册

--- src/js/register.js
+++ src/js/register.js
@@ -1,2 +1,15 @@
 import "../less/normalized.less"
 import "../less/header-footer.less"
+import "../less/register.less"
+
+
+$(".btnTwo").click(function(){
+    $(".renone").addClass("equally")
+    $(".rentwo").removeClass("equally")
+})
+
+$(".btnThree").click(function(){
+    $(".rentwo").addClass("equally")
+    $(".renone").removeClass("equally")
+})
+
