43b0f24164b66df371df59757e1dc819cf2a8432
kong_1995 <1132154220@qq.com>
2020-01-05 20:57:47 +0800

    列表修改

--- src/js/productsList.js
+++ src/js/productsList.js
@@ -1,3 +1,9 @@
 import "../less/normalized.less"
 import "../less/header-footer.less"
 import "../less/productsList.less"
+
+$().ready(function(){
+    $(".content_left button").click(function(){
+        $(this).addClass("selection").siblings().removeClass("selection")
+    })
+})
