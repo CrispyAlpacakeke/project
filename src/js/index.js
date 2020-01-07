af2034b839b37445bef5b60f2823a1630322276c
CrispyAlpacakeke <1589886936@qq.com>
2020-01-07 20:41:07 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,19 @@ import '../less/index.less'
 import Swiper from 'swiper';
 import 'swiper/css/swiper.css'
 
+// /**
+//   项目JS主入口
+//   以依赖layui的layer和form模块为例
+// **/    
+// layui.define(['layer', 'form'], function(exports){
+//     var layer = layui.layer
+//     ,form = layui.form;
+    
+//     layer.msg('Hello World');
+    
+//     exports('index', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
+//   });  
+
 /****导航栏二级菜单下滑效果****/
 let time={}
 $('.nav-item .link').mouseover(function(){
